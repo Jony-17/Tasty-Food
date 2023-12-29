ec73c89dde4387fc5e75a7d60448d44f9e97f3ee
João Araújo <73491681+Jony-17@users.noreply.github.com>
2023-12-29 23:26:11 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};*/
+
+
+const menu_btn = document.querySelector('.hamburguer');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+});
